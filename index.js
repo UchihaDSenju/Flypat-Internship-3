06e5b789b8789160565684fa76d87a947fe22063
Tariq <rafiqtariq19@gmail.com>
2023-03-10 23:05:04 +0530

    localStorage Added in project

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const inputText = document.querySelector('.inputTag');
 const tasks = document.querySelector('.tasks');
 const addBtn = document.querySelector('.addTask');
-if(!localStorage['id']){
+const demoBtn = document.querySelector('.demo');
+var ids = [];
+var id = 0;
+
+
+demoBtn.addEventListener('click', () => {
+    localStorage.clear();
     localStorage.setItem('id', 8051);
+    location.reload();
+})
+
+if(!localStorage['id']){
+    id = 8051;
 }
-var id = parseInt(localStorage['id']);
+else{
+    id = parseInt(localStorage['id']);
+}
+if(localStorage['idArray']){
+    ids = JSON.parse(localStorage.getItem('idArray'));
+}
+
+for(var i = 0; i < ids.length; i++){
+    console.log(ids[i]);
+}
+
 
 inputText.addEventListener('keyup', (e) => {
     if(e.key == 'Enter'){
@@ -16,6 +37,7 @@ inputText.addEventListener('keyup', (e) => {
             inputText.value = "";
         }
     }
+    
 })
 
 addBtn.addEventListener('click', () => {
@@ -26,7 +48,8 @@ addBtn.addEventListener('click', () => {
         addItem(inputText.value);
         inputText.value = '';
     }
-})
+});
+
 // Add Function
 const addItem = (item) => {
     const listItem = document.createElement('li');
@@ -38,14 +61,49 @@ const addItem = (item) => {
     <span>${item}</span>
     <i class="far fa-trash-alt"></i>
     `;
-
+    ids.push(id)
+    localStorage.setItem('idArray', JSON.stringify(ids));
     listItem.addEventListener('click', () => {
         listItem.querySelector('span').classList.toggle('done');
-    })
+    });
 
     listItem.querySelector('i').addEventListener('click', () => {
         console.log(listItem.id);
+        localStorage.removeItem("8052");
         listItem.remove();
-    })
+    });
+    localStorage.setItem(id, item);
     tasks.appendChild(listItem);
+    location.reload();
 }
+
+//get items from localStorage Function
+const getItem = () => {
+    for(var i = 0; i < ids.length; i++){
+        const listItem = document.createElement('li');
+        listItem.id = ids[i];
+        listItem.className = 'task';
+        var content = localStorage.getItem(ids[i]);
+        listItem.innerHTML = `
+        <span>${content}</span>
+        <i class="far fa-trash-alt"></i>
+        `;
+        listItem.addEventListener('click', () => {
+            listItem.querySelector('span').classList.toggle('done');
+        });
+
+        listItem.querySelector('i').addEventListener('click', () => {
+            console.log(listItem.id);
+            ids.splice(ids.indexOf(parseInt(listItem.id)), 1);
+            console.log(ids);
+            localStorage.removeItem(parseInt(listItem.id))
+            localStorage.setItem('idArray', JSON.stringify(ids));
+            listItem.remove();
+        });
+        tasks.appendChild(listItem);
+    }
+}
+
+// Gets Everything from the localStorage when user visits the page again
+getItem();
+
